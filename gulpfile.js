f7823629823496e4d37aa3575ab27b520b9d5fa3
Vadim Komolov <vkomolov.ua@gmail.com>
2024-05-28 19:04:09 +0300

    to make exported functions on gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,6 @@ import * as dartSass from "sass";
 import gulpSass from "gulp-sass";
 import sassGlob from "gulp-sass-glob";
 import clean from "gulp-clean";
-import fs from "fs";
 
 import LocalServer from "./src/modules/localServer.js";
 
@@ -36,7 +35,7 @@ const pathData = {
     watch: {
         html: `${ srcPath }**/*.html`,
         styles: `${ srcPath }scss/**/*.scss`,
-        js: `${ srcPath }js/**/*.js`,
+        js: `${ srcPath }js/**/*.js ${srcPath}modules/**/*.js`,
         img: `${ srcPath }assets/img/**/*.{jpg,png,svg,gif,ico,webp,xml,json,webmanifest}`,
         fonts: `${ srcPath }assets/fonts/**/*.{eot,woff,woff2,ttf,otf}`,
         data: `${ srcPath }assets/data/**/*.json`,  //is necessary only for watching: data will not be in "dist"
@@ -47,21 +46,27 @@ const pathData = {
 const sass = gulpSass(dartSass);
 const localServer = new LocalServer(pathData.build.html);
 
-function includeHtml() {
+function handleHtml() {
     return src(pathData.src.html)
         .pipe(fileInclude(fileIncludeSettings))
         .pipe(dest(pathData.build.html))
 }
-
 function handleStyles() {
     return src(pathData.src.styles)
         .pipe(sass({}, () => {}))
         .pipe(dest(pathData.build.styles));
 }
-
 function handleImages() {
     return src(pathData.src.img)
         .pipe(dest(pathData.build.img));
+}
+
+function watchFiles() {
+    gulp.watch(pathData.watch.html, handleHtml);
+    gulp.watch(pathData.watch.styles, handleStyles);
+    //gulp.watch(pathData.watch.js, handleJs);
+    gulp.watch(pathData.watch.img, handleImages);
+    //gulp.watch(pathData.watch.fonts, handleFonts);
 }
 
 export function cleanDist() {
@@ -75,7 +80,25 @@ export function cleanDist() {
         .pipe(clean({ force: true }));
 }
 
-export const testStream = gulp.series(handleStyles, localServer.stream);
+//TODO: to accomplish runBuild
+export function runBuild(cb) {
+    gulp.series(
+        cleanDist,
+        gulp.parallel(
+            handleHtml,
+            handleStyles,
+            handleImages,
+        )
+    )(cb);
+}
+export function runDev(cb) {
+    gulp.series(
+        runBuild,
+        localServer.reload,
+        watchFiles
+    )(cb);
+}
 
-export const testReload = gulp.series(includeHtml, localServer.reload);
+//export const testStream = gulp.series(handleStyles, localServer.stream);
+//export const testReload = gulp.series(handleHtml, localServer.reload);
 
